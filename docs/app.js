6a6451f92f07ab8f11cbc9c5e2eaf7807c8e736d
smari steinar <smarikul@hotmail.com>
2020-09-03 18:25:49 +0000

    buinn

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,20 @@
 
+
+
 function blae(){
-    let blen = document.getElementById("blaa");
-    blen.style.opacity = 0;
+    let geimverEl = document.getElementById("geimvera");
+    let sprengjaEl = document.getElementById("sprengja");
+
+    console.log(window.getComputedStyle(geimverEl).getPropertyValue('opacity'));
+    if (window.getComputedStyle(geimverEl).getPropertyValue('opacity') == 1)              
+    {  
+       sprengjaEl.style.opacity = 1;
+       geimverEl.style.opacity = 0; 
+    }
+    else{
+        sprengjaEl.style.opacity = 0;
+        geimverEl.style.opacity = 1; 
+    }
 }
 
 
